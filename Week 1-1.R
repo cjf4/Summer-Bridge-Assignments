02056040df626e012bedd79c20f4240d0180fe9c
Chris Fenton <christopherfenton4@gmail.com>
2015-07-05 14:14:10 -0400

    Assignment Complete

--- Week 1-1.R
+++ Week 1-1.R
@@ -1,3 +1,5 @@
+# 1. Write a loop that calculates 12-factorial
+
 twelve <- 1:12
 x <- 1
 
@@ -6,3 +8,18 @@ for(i in twelve){
   }
 
 print(x)
+
+# 2. Show how to create a numeric vector that contains the sequence 20-50 by 5:
+
+nvect <- seq(20,50,5)
+
+# 3. Show how to take a trio of input numbers a, b, and c and implement the
+#     quadratic equation:
+
+quadratic_e <- function(a, b, c){
+  root_of_d <- sqrt((b^2 - 4 * a * c))
+  root_1 <- (-1 * b + root_of_d)/2*a
+  root_2 <- (-1 * b - root_of_d)/2*a
+  return(c(root_1,root_2))
+}
+
