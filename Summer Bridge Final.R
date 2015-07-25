44a2dfaaae423a2456a095ca9d451a327fc0c895
Chris Fenton <christopherfenton4@gmail.com>
2015-07-25 10:32:54 -0400

    fixed nested for loop, have salary data in single DF

--- Summer Bridge Final.R
+++ Summer Bridge Final.R
@@ -11,7 +11,7 @@ nfl_teams <- c("bills", "dolphins", "jets", "patriots",
                "falcons", "panthers", "saints", "buccaneers",
                "rams", "seahawks", "49ers", "cardinals")
 
-years <- c( "2001", "2002", "2003", "2004", "2005", 
+years <- c("2002", "2003", "2004", "2005", 
            "2006", "2007", "2008", "2009-10")
 
 salary_data <- data.frame()
@@ -32,31 +32,12 @@ download_data <- function(team, year, df) {
 }
 
 #getting error, put try catch on rbind right above
-for (i in nfl_teams) {
-  for ( j in years) {
+for (i in 1:32) {
+  for ( j in 1:8) {
     salary_data <- download_data(nfl_teams[i], years[j], salary_data)
   }
 }
 
-#data is in a table, and labelled as a list element
-giants <- readHTMLTable('http://content.usatoday.com/sportsdata/football/nfl/Giants/salaries/2009-10')
-
-#double bracket to access the first element of the list directly
-bsals <- giants[['team-salaries']]
-
-#turn the first 6 col vectors into character vectors, leave the 7th as a factor
-#glue them back together
-bsals <- cbind(lapply(bsals[1:6], as.character), bsals[7], stringsAsFactors = FALSE)
-
-#prepare the numeric data to be converted via regex removal of non digits
-#change this to a loop or lapply
-bsals$'BASE SALARY' <- as.numeric(gsub("[^[:digit:]]", "", bsals$'BASE SALARY'))
-bsals$'SIGN BONUS' <- as.numeric(gsub("[^[:digit:]]", "", bsals$'SIGN BONUS'))
-bsals$'ALL BONUSES' <- as.numeric(gsub("[^[:digit:]]", "", bsals$'ALL BONUSES'))
-bsals$'CAP VALUE' <- as.numeric(gsub("[^[:digit:]]", "", bsals$'CAP VALUE'))
-bsals$'SALARY' <- as.numeric(gsub("[^[:digit:]]", "", bsals$'SALARY'))
-
-str(bsals)
 
 #next steps
 #loop through url to download all teams/seasons
